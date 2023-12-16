544ee6f2911d52cfbe0355a983eac75a1e974291
Maxime Curon <114878203+bterpperoni@users.noreply.github.com>
2023-12-16 10:12:35 +0100

    Separate Mainte from Layout and fiximport position issues

--- src/lib/components/layout/MainHtml.tsx
+++ src/lib/components/layout/MainHtml.tsx
@@ -4,7 +4,7 @@ export default function MainHtml({ children }: { children: React.ReactNode }) {
 
     return (
         <>
-        <main className=" flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <main className=" flex flex-col bg-gradient-to-b from-[#2e026d] to-[#15162c]">
                 {children}
         </main>
         </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,17 +17,20 @@ export default function Home() {
     <>
     <LayoutMain>
         <MainHtml>
+         <section className="flex flex-col min-h-screen items-center justify-center">
           <h1 className="text-6xl text-white">CARHEH</h1>
-            <h2 className="text-2xl text-white">Carpooling for students from Mons</h2>
-              <div className="flex flex-col items-center">
-                  {!session && <Button className="mt-4 rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20" onClick={() => void signIn()}>Sign in</Button>}
-                  {session && (
-                    <>
-                      <h1>Logged in as {session.user.name}</h1>
-                      <h2>Session expires in {session.expires}</h2>
-                    </>
-                  )}
-              </div>
+              <h2 className="text-2xl text-white">Carpooling for students from Mons</h2>
+                <div className="flex flex-col items-center">
+                    {!session && <Button className="mt-4 rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20" onClick={() => void signIn()}>Sign in</Button>}
+                    {session && (
+                      <>
+                        <h1>Logged in as {session.user.name}</h1>
+                        <h2>Session expires in {session.expires}</h2>
+                      </>
+                    )}
+                </div>
+          </section> 
+          
           </MainHtml>        
       </LayoutMain>
     </>
