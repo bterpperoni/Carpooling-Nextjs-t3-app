91df4bab4e1e6f9dc2c72279f92d814bfd170bc1
Maxime Curon <114878203+bterpperoni@users.noreply.github.com>
2024-03-28 19:18:38 +0100

    create button icon Infos

--- src/pages/social/groups/[group]/index.tsx
+++ src/pages/social/groups/[group]/index.tsx
@@ -6,6 +6,7 @@ import LayoutMain from '$/lib/components/layout/LayoutMain';
 import { useState } from "react";
 import GroupForm from "$/lib/components/form/GroupForm";
 import { getCampusAbbr } from "$/utils/data/school";
+import Infos from "$/lib/components/button/Infos";
 
 /* ------------------------------------------------------------------------------------------------------------------------
 ------------------------- Page to display a specifig group ----------------------------------------------------------------  
@@ -55,19 +56,13 @@ if(sessionData)
                     <div className="flex justify-center">                    
                         <div className="bg-white w-[90vw] h-max pb-8 flex flex-col items-center">
                             <div className=" flex flex-row items-center">
-                                <div    className="cursor-pointer mx-4 rounded-full border-2 border-black p-2 hover:border-[var(--pink-g1)]"
-                                        onClick={() => setIsInfos(true)}>
+                                <div    
+                                    className="cursor-pointer 
+                                               mx-4 rounded-full border-2 
+                                               border-black p-2 
+                                               hover:border-[var(--pink-g1)]">
                                 {/* ---------------------------------------------- Icon infos ----------------------------------------------------- */}
-                                    <svg fill="#000000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
-                                    width="30px" height="30px" viewBox="0 0 416.979 416.979">
-                                        <g>
-                                            <path d="M356.004,61.156c-81.37-81.47-213.377-81.551-294.848-0.182c-81.47,81.371-81.552,213.379-0.181,294.85
-                                            c81.369,81.47,213.378,81.551,294.849,0.181C437.293,274.636,437.375,142.626,356.004,61.156z M237.6,340.786
-                                            c0,3.217-2.607,5.822-5.822,5.822h-46.576c-3.215,0-5.822-2.605-5.822-5.822V167.885c0-3.217,2.607-5.822,5.822-5.822h46.576
-                                            c3.215,0,5.822,2.604,5.822,5.822V340.786z M208.49,137.901c-18.618,0-33.766-15.146-33.766-33.765
-                                            c0-18.617,15.147-33.766,33.766-33.766c18.619,0,33.766,15.148,33.766,33.766C242.256,122.755,227.107,137.901,208.49,137.901z"/>
-                                        </g>
-                                    </svg>
+                                    <Infos wIcon={30} hIcon={30} handleInfos={() => setIsInfos(true)} />
                                 {/* ------------------------------------------------------------------------------------------------- */}
                                 </div>
                                 <div className="border-2 text-white p-3 bg-[var(--purple-g2)] my-2 mx-4">
